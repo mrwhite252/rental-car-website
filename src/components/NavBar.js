b39695a1b5b00afd0c359ce49034513aae7234c7
DavidBai <baiminghan@gmail.com>
2021-12-06 11:34:56 +1300

    initial setup

---
+++ src/components/NavBar.js
@@ -0,0 +1,3 @@
+export default function NavBar() {
+  return <div>this is nav bar</div>;
+}
---
+++ src/views/HomeView.js
@@ -0,0 +1,10 @@
+import NavBar from "../components/NavBar";
+
+export default function HomeView() {
+  return (
+    <div>
+      this is the home view
+      <NavBar />
+    </div>
+  );
+}
